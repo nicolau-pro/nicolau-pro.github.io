cfe2746d77edcd9e679ec3c3b17a8df2f6418041
radu-colibri <radu.alex.nicolau@gmail.com>
2025-05-29 07:50:02 +0100

    Post process

---
+++ src/post.py
@@ -0,0 +1,65 @@
+import bs4
+import time
+import io
+from pathlib import Path
+
+
+def replace_all(str, search, replace):
+
+    s = str.split(search)
+    new_str = ""
+
+    for i, text in enumerate(s):
+        new_str += text
+        if i < len(s) - 1:
+            new_str += replace
+
+    return new_str
+
+
+def remove_react_libraries(html):
+    time.sleep(0.02)
+    # load the file
+    try:
+        with io.open(html) as inf:
+            txt = inf.read()
+            soup = bs4.BeautifulSoup(txt, "html.parser")
+    except:
+        with io.open(html, encoding="utf8") as inf:
+            txt = inf.read()
+            soup = bs4.BeautifulSoup(txt, "html.parser")
+
+    # Remove index-????????.js script
+    for script in soup.find_all("script"):
+        if "/assets/index-" in str(script):
+            script.decompose()
+
+    # Save the file
+    with open(html, "w") as outf:
+        txt = replace_all(
+            str(soup), "/>", ">"
+        )  # Remove trailing slash in empty html elements
+        outf.write(txt)
+
+
+total_pages = 0
+
+# Count the pages:
+for path in Path(".presite").rglob("index.html"):
+    total_pages += 1
+
+# Process pages and add to sitemap:
+for i, page in enumerate(Path(".presite").rglob("index.html")):
+    remove_react_libraries(page)
+
+    print(
+        "✅ Processed "
+        + str(i + 1)
+        + " of "
+        + str(total_pages)
+        + " ("
+        + str(page)
+        + ")"
+    )
+
+print("🟢 ALL DONE")
