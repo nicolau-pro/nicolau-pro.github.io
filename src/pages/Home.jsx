00df2ad22a19b1e5f29661985fffe6fc672d4081
radu-colibri <radu.alex.nicolau@gmail.com>
2025-06-02 11:13:05 +0100

    integrate back end

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import React, { useEffect, useState } from "react";
 import Row from "../components/layout/Row";
 import Col from "../components/layout/Col";
 import Section from "../components/layout/Section";
 
 function Page() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Error fetching users:", err));
+  }, []);
+
   return (
     <>
       <title>Home - Radu Nicolau</title>
@@ -15,6 +25,23 @@ function Page() {
               <h1 className="hero">Home</h1>
             </Col>
           </Row>
+          {users.length > 0 && (
+            <Row>
+              <Col>
+                <h2 className="hero">Users</h2>
+                <ul>
+                  {users.map((user) => (
+                    <li key={user.id}>
+                      <strong>
+                        {user.firstName} {user.lastName}
+                      </strong>
+                      , {user.age} - {user.email}
+                    </li>
+                  ))}
+                </ul>
+              </Col>
+            </Row>
+          )}
         </Section>
       </main>
     </>
